31565fdb45946bdce4f876e8fffedc2512d1a322
JSifalda <sifalda.jiri@gmail.com>
2012-12-09 14:36:06 +0100

    Add Source

---
+++ Flame/Facebook/UserService.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * UserService.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    05.11.12
+ */
+
+namespace Flame\Facebook;
+
+class UserService extends \Nette\Object
+{
+
+	/**
+	 * @var string
+	 */
+	private $user;
+
+	/**
+	 * @var \Facebook $facebook
+	 */
+	private $facebook;
+
+	/**
+	 * @param \Facebook $facebook
+	 */
+	public function __construct(\Facebook $facebook)
+	{
+		$this->facebook = $facebook;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUser()
+	{
+		if($this->user === null)
+			$this->user = $this->facebook->getUser();
+
+		return $this->user;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getData()
+	{
+		if(!$this->getUser()) return;
+
+		try {
+			return $this->facebook->api($this->user);
+		}catch (\FacebookApiException $ex){
+			$this->user = null;
+		}
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getAvatarUrl()
+	{
+		if(!$this->getUser()) return;
+
+		try {
+			$fields = $this->facebook->api($this->user, array(
+				'fields' => 'picture',
+				'type'   => 'large'
+			));
+
+			if(isset($fields['picture']['data']['url']))
+				return $fields['picture']['data']['url'];
+
+		}catch (\FacebookApiException $ex){
+			$this->user = null;
+		}
+	}
+
+	/**
+	 * @param $key
+	 * @param null $default
+	 * @return null
+	 */
+	public function getDataBy($key, $default = null)
+	{
+		$api = $this->getUserData();
+		return (isset($api[$key])) ? $api[$key] : $default;
+	}
+
+	/**
+	 * @param null $redirect
+	 * @return string
+	 */
+	public function getLoginurl($redirect = null)
+	{
+		$params = array('scope' => 'email');
+		if($redirect) $params += array('redirect_uri' => $redirect);
+		return $this->facebook->getLoginUrl($params);
+	}
+
+	/**
+	 * @return \Facebook
+	 */
+	public function getFacebookService()
+	{
+		return $this->facebook;
+	}
+
+}
